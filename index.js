42ca422956f117467bf787088ed100d9208dba27
Iaroslav Bezruchko <i_bezruchko@wargaming.net>
2023-04-13 16:41:32 +0300

    Ability to upload and view csvs

--- index.js
+++ index.js
@@ -1,71 +1,99 @@
 import * as d3 from "d3";
 import * as Papa from "papaparse";
 
+function handleFileSelect(event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (event) {
+    const csvData = event.target.result;
+    visualizeData(csvData);
+  };
+
+  reader.readAsText(file);
+}
+
 // Load the CSV file and process the data
-Papa.parse("./data.csv", {
-  download: true,
-  header: true,
-  dynamicTyping: true,
-  complete: function (results) {
-    const data = results.data;
+function visualizeData(data) {
+  Papa.parse(data, {
+    header: true,
+    dynamicTyping: true,
+    complete: function (results) {
+      const data = results.data;
 
-    // Group the data by date and count the number of registrations for each date
-    const groupedData = d3.rollup(
-      data,
-      (v) => d3.sum(v, (d) => d.regs),
-      (d) =>
-        new Date(d.date).toLocaleDateString("en-US", {
-          month: "short",
-          day: "numeric",
+      // Group the data by date and count the number of registrations for each date
+      const groupedData = d3.rollup(
+        data,
+        (v) => d3.sum(v, (d) => d.regs),
+        (d) =>
+          new Date(d.date).toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+          })
+      );
+      // Convert the grouped data to an array of objects with "date" and "registrations" properties
+      const formattedData = Array.from(
+        groupedData,
+        ([date, registrations]) => ({
+          date: date,
+          registrations: registrations,
         })
-    );
-    // Convert the grouped data to an array of objects with "date" and "registrations" properties
-    const formattedData = Array.from(groupedData, ([date, registrations]) => ({
-      date: date,
-      registrations: registrations,
-    }));
+      ).reverse();
 
-    // Define the dimensions and margins for the histogram
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const width = 1280 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+      // Define the dimensions and margins for the histogram
+      const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+      const width = 1280 - margin.left - margin.right;
+      const height = 500 - margin.top - margin.bottom;
 
-    // Define the scales for the x and y axes
-    const x = d3.scaleBand().range([0, width]).padding(0.1);
-    const y = d3.scaleLinear().range([height, 0]);
+      // Define the scales for the x and y axes
+      const x = d3.scaleBand().range([0, width]).padding(0.1);
+      const y = d3.scaleLinear().range([height, 0]);
+      // Remove the old SVG element
+      d3.select("#histogram svg").remove();
+      // Create the SVG element and set its dimensions
+      const svg = d3
+        .select("#histogram")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // Create the SVG element and set its dimensions
-    const svg = d3
-      .select("#histogram")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+      // Set the domains for the x and y scales
+      x.domain(formattedData.map((d) => d.date));
+      y.domain([0, d3.max(formattedData, (d) => d.registrations)]);
 
-    // Set the domains for the x and y scales
-    x.domain(formattedData.map((d) => d.date));
-    y.domain([0, d3.max(formattedData, (d) => d.registrations)]);
+      // Add the x axis to the SVG element
+      svg
+        .append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
 
-    // Add the x axis to the SVG element
-    svg
-      .append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x));
+      // Add the y axis to the SVG element
+      svg.append("g").call(d3.axisLeft(y));
 
-    // Add the y axis to the SVG element
-    svg.append("g").call(d3.axisLeft(y));
+      // Add the bars to the SVG element
+      svg
+        .selectAll(".bar")
+        .data(formattedData)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => x(d.date))
+        .attr("y", (d) => y(d.registrations))
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => height - y(d.registrations));
+    },
+  });
+}
 
-    // Add the bars to the SVG element
-    svg
-      .selectAll(".bar")
-      .data(formattedData)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", (d) => x(d.date))
-      .attr("y", (d) => y(d.registrations))
-      .attr("width", x.bandwidth())
-      .attr("height", (d) => height - y(d.registrations));
-  },
+// Load the local CSV file and process the data
+d3.csv("./data.csv").then(function (data) {
+  const csvData = Papa.unparse(data);
+  visualizeData(csvData);
 });
+
+// Listen for file input changes
+document
+  .getElementById("file-input")
+  .addEventListener("change", handleFileSelect, false);
